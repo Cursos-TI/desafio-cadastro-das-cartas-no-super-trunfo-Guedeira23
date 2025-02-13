479cd942e75698723fb60524f115271d72e09709
Guedeira23 <lucas.guedesdf8@gmail.com>
2025-02-13 13:43:16 +0000

    finalizei com os comentarios

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -23,27 +23,27 @@ int main() {
     char nome_cidade[20];
     float populacao, area, pib;
 
-    // irei pedir o código da cidade e declarar ele com um printf.
+    // irei pedir o código da cidade.
     printf("digite o código da cidade:\n");
     scanf("%d", &cod_cidade);
 
-    //irei digitar o nome da cidade e imprimir esse nome com o printf.
+    //irei digitar o nome da cidade.
     printf("digite o nome da cidade:\n");
     scanf("%s", &nome_cidade);
 
-    //irei declarar a população e imprimir com o printf.
+    //irei declarar a população.
     printf("digite a população da cidade:\n");
     scanf("%f", &populacao);
-
+    //irei declarar a área da cidade.
     printf("digite a àrea da cidade:\n");
     scanf("%f", &area);
-
+    //irei declarar o pib da cidade.
     printf("digite o pib da cidade:\n");
     scanf("%f", &pib);
-
+    //irei digitar a quantidade de pontos turisticos da cidade.
     printf("digite a quantidade de pontos turisticos da cidade:\n");
     scanf("%d", &pontos_turisticos);
-
+    //irei imprimir todos os resultados de uma vez com o printf. Sendo um em cada linha com o \n.
     printf("O código da cidade é: %d\n O nome da cidade é: %s\n A população da cidade é: %f\n A área da cidade é: %f\n O pib da cidade é: %f\n Os pontos turisticos da cidade são: %d\n ", cod_cidade, nome_cidade, populacao, area, pib, pontos_turisticos);
 
     return 0;
